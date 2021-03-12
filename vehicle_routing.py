c546846f1e687396f11c4e0d0f2bb61c492ae52d
Aidan <aidanvickars@gmail.com>
2021-03-12 13:48:15 -0800

    completed time windows routing

--- vehicle_routing.py
+++ vehicle_routing.py
@@ -1,40 +1,47 @@
 import multiprocessing as mp
 import numpy as np
 import pandas as pd
-import time
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
 
-def create_data_model(df, numVehicles, depotLocation):
+def create_data_model(timeMatrix, numVehicles, depot, timeWindows):
     """Stores the data for the problem."""
-    data = {'distance_matrix': df.to_numpy(), 'num_vehicles': numVehicles, 'depot': depotLocation}
+    data = {}
+    data['time_matrix'] = timeMatrix.to_numpy()
+    data['time_windows'] = timeWindows
+    data['num_vehicles'] = numVehicles
+    data['depot'] = depot
     return data
 
 
-def print_solution(data, manager, routing, solution,processNumber):
-    # CITATION: https://developers.google.com/optimization/routing/vrp
+def print_solution(data, manager, routing, solution, processNumber):
     routes = {}
     """Prints solution on console."""
-    max_route_distance = 0
+    time_dimension = routing.GetDimensionOrDie('Time')
+    total_time = 0
     for vehicle_id in range(data['num_vehicles']):
-        routes[vehicle_id] = []
+        routes[vehicle_id] = pd.DataFrame(columns=['node', 'time'])
         index = routing.Start(vehicle_id)
         plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
-        route_distance = 0
         while not routing.IsEnd(index):
-            # plan_output += ' {} -> '.format(manager.IndexToNode(index))
-            routes[vehicle_id].append(manager.IndexToNode(index))
-            previous_index = index
+            time_var = time_dimension.CumulVar(index)
+            routes[vehicle_id] = routes[vehicle_id].append({'node': manager.IndexToNode(index), 'time': solution.Min(time_var)}, ignore_index=True)
+            # plan_output += '{0} Time({1},{2}) -> '.format(
+            #     manager.IndexToNode(index), solution.Min(time_var),
+            #     solution.Max(time_var))
             index = solution.Value(routing.NextVar(index))
-            route_distance += routing.GetArcCostForVehicle(
-                previous_index, index, vehicle_id)
-        # plan_output += '{}\n'.format(manager.IndexToNode(index))
-        routes[vehicle_id].append(manager.IndexToNode(index))
-        plan_output += 'Distance of the route: {}s\n'.format(route_distance)
-        print('Process:',processNumber, plan_output)
-        max_route_distance = max(route_distance, max_route_distance)
-    print('Maximum of the route distances: {}s'.format(max_route_distance))
+        time_var = time_dimension.CumulVar(index)
+        routes[vehicle_id] = routes[vehicle_id].append({'node': manager.IndexToNode(index), 'time': solution.Min(time_var)}, ignore_index=True)
+        # plan_output += '{0} Time({1},{2})\n'.format(manager.IndexToNode(index),
+        #                                             solution.Min(time_var),
+        #                                             solution.Max(time_var))
+        plan_output += 'Time of the route: {}min\n'.format(
+            solution.Min(time_var))
+        print('Process:', processNumber, solution.Min(time_var))
+        #         print(plan_output)
+        total_time += solution.Min(time_var)
+    print('Total time of all routes: {}min'.format(total_time))
     return routes
 
 
@@ -55,88 +62,129 @@ def solver_status(status):
 
 def route_names(routes, locations):
     namedRoutes = {}
+    times = {}
     for vehicle in routes.keys():
-        namedRoutes[vehicle] = locations[routes[vehicle]]
-    return namedRoutes
+        namedRoutes[vehicle] = locations[list(routes[vehicle]['node'].values)]
+        times[vehicle] = routes[vehicle]['time'].values
+    return namedRoutes, times
 
 
 def tableau_route(routes, locations, timeMatrixFiltered):
-    namedRoutes = route_names(routes, timeMatrixFiltered.index)
+    namedRoutes, times = route_names(routes, timeMatrixFiltered.index)
 
-    tableauRoutes = pd.DataFrame(columns={'address', 'latitude', 'longitude', 'pairID', 'vehicleID'})
+    tableauRoutes = pd.DataFrame(columns={'address', 'latitude', 'longitude', 'pairID', 'vehicleID', 'time'})
     for vehicle in namedRoutes.keys():
         for vertex in range(len(namedRoutes[vehicle]) - 1):
             currentVertexInfo = locations[locations.index == namedRoutes[vehicle][vertex]]
+            currentVertexTime = times[vehicle][vertex]
             currentVertex = {'address': currentVertexInfo.index[0],
                              'latitude': currentVertexInfo['lat'].values[0],
                              'longitude': currentVertexInfo['long'].values[0],
                              'pairID': vertex,
-                             'vehicleID': vehicle}
+                             'vehicleID': vehicle,
+                             'time': currentVertexTime}
 
             nextVertexInfo = locations[locations.index == namedRoutes[vehicle][vertex + 1]]
+            nextVertexTime = times[vehicle][vertex + 1]
 
             nextVertex = {'address': nextVertexInfo.index[0],
                           'latitude': nextVertexInfo['lat'].values[0],
                           'longitude': nextVertexInfo['long'].values[0],
                           'pairID': vertex,
-                          'vehicleID': vehicle}
+                          'vehicleID': vehicle,
+                          'time': nextVertexTime}
 
             tableauRoutes = tableauRoutes.append([currentVertex, nextVertex], ignore_index=True)
     return tableauRoutes
 
 
-def create_route(timeMatrixFiltered, numVehicles, depotLocation, routeLimit, locations, processNumber, zone):
+def time_windows(zone, timeMatrixFiltered, maxTimeWindow):
+    timeWindows = []
+    if zone == 1:
+        for i in range(len(timeMatrixFiltered)):
+            house = timeMatrixFiltered.index[i].split(' ')
+            street = ' '.join(house[1:-4])
+
+            if street == 'LINCOLN AVE,':
+                timeWindows.append((0, maxTimeWindow))
+            else:
+                timeWindows.append((0, 50000))
+
+    return timeWindows
+
+
+def create_route(timeMatrixFiltered, numVehicles, depotLocation, routeLimit, locations, processNumber, zone, maxTimeWindow):
     # CITATION: https://developers.google.com/optimization/routing/vrp
     """Solve the CVRP problem."""
     print("In thread", processNumber)
     # Instantiate the data problem.
+
+    # Creating the time windows
+    timeWindows = time_windows(zone, timeMatrixFiltered, maxTimeWindow)
+
     # data = create_data_model(timeMatrixFiltered, numVehicles, depotLocation)
-    data = create_data_model(timeMatrixFiltered, numVehicles, int(depotLocation))
+    data = create_data_model(timeMatrixFiltered, numVehicles, int(depotLocation), timeWindows)
     # For some reason you have to make depotLocation an int, otherwise it throws an error
 
     # Create the routing index manager.
-    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+    manager = pywrapcp.RoutingIndexManager(len(data['time_matrix']),
                                            data['num_vehicles'], data['depot'])
 
     # Create Routing Model.
     routing = pywrapcp.RoutingModel(manager)
 
     # Create and register a transit callback.
-    def distance_callback(from_index, to_index):
-        """Returns the distance between the two nodes."""
-        # Convert from routing variable Index to distance matrix NodeIndex.
+    def time_callback(from_index, to_index):
+        """Returns the travel time between the two nodes."""
+        # Convert from routing variable Index to time matrix NodeIndex.
         from_node = manager.IndexToNode(from_index)
         to_node = manager.IndexToNode(to_index)
-        return data['distance_matrix'][from_node][to_node]
+        return data['time_matrix'][from_node][to_node]
 
-    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    transit_callback_index = routing.RegisterTransitCallback(time_callback)
 
     # Define cost of each arc.
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-    # Add Distance constraint.
-    dimension_name = 'Distance'
+    # Add Time Windows constraint.
+    time = 'Time'
     routing.AddDimension(
         transit_callback_index,
-        0,  # no slack
-        routeLimit,  # vehicle maximum travel distance # 5000
-        True,  # start cumul to zero
-        dimension_name)
-    distance_dimension = routing.GetDimensionOrDie(dimension_name)
-    distance_dimension.SetGlobalSpanCostCoefficient(100)
+        30,  # allow waiting time
+        routeLimit,  # maximum time per vehicle
+        False,  # Don't force start cumul to zero.
+        time)
+    time_dimension = routing.GetDimensionOrDie(time)
+
+    # Add time window constraints for each location except depot.
+    for location_idx, time_window in enumerate(data['time_windows']):
+        if location_idx == data['depot']:
+            continue
+        index = manager.NodeToIndex(location_idx)
+        time_dimension.CumulVar(index).SetRange(time_window[0], time_window[1])
+    # Add time window constraints for each vehicle start node.
+    depot_idx = data['depot']
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        time_dimension.CumulVar(index).SetRange(
+            data['time_windows'][depot_idx][0],
+            data['time_windows'][depot_idx][1])
+
+    # Instantiate route start and end times to produce feasible times.
+    for i in range(data['num_vehicles']):
+        routing.AddVariableMinimizedByFinalizer(
+            time_dimension.CumulVar(routing.Start(i)))
+        routing.AddVariableMinimizedByFinalizer(
+            time_dimension.CumulVar(routing.End(i)))
 
     # Setting first solution heuristic.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
     search_parameters.first_solution_strategy = (
         routing_enums_pb2.FirstSolutionStrategy.SAVINGS)
-    # search_parameters.local_search_metaheuristic = (
-    #     routing_enums_pb2.LocalSearchMetaheuristic.AUTOMATIC)
 
     search_parameters.time_limit.seconds = 300
 
     search_parameters.solution_limit = 1
-
-    #     search_parameters.lns_time_limit.seconds = 1
 
     # Solve the problem.
     print(f"Process: {processNumber}, starting")
@@ -151,7 +199,7 @@ def create_route(timeMatrixFiltered, numVehicles, depotLocation, routeLimit, loc
     tableauRoute = tableau_route(routes, locations, timeMatrixFiltered)
 
     print(f"Process: {processNumber}, saving: ")
-    # tableauRoute.to_csv(f"zone{zone}_{numVehicles}trucks.csv")
+    tableauRoute.to_csv(f"zone{zone}_{numVehicles}trucks.csv")
 
 
 def main():
@@ -162,6 +210,7 @@ def main():
     print("Reading in time matrix")
     timeMatrix = pd.read_csv('C:/Users/Aidan/OneDrive - Simon Fraser University (1sfu)/Garbage Route Optimization/timeMatrixInflated.csv',
                              index_col=0)
+    timeMatrix = timeMatrix.iloc[timeMatrix.index != 'NO ADDRESS, Port Coquitlam, BC, Canada', timeMatrix.index != 'NO ADDRESS, Port Coquitlam, BC, Canada']  # Removing the pesky no address
 
     # Reading in locations by zones
     print("Reading in locations by zone")
@@ -171,11 +220,13 @@ def main():
     # Reading in all locations
     print("Reading in locations")
     locations = pd.read_csv('C:/Users/Aidan/OneDrive - Simon Fraser University (1sfu)/Garbage Route Optimization/locations.csv', index_col=0)
+    locations = locations[locations.index != 'NO ADDRESS, Port Coquitlam, BC, Canada']  # Removing the pesky no address
 
     print('Dividing up the time matrix by zone')
     # Dividing up the time matrix by zone
     timeMatrixFiltered = {}  # Contains the time matrix
     depotLocations = {}  # Contains the depot location in each subdivided time matrix
+    timeWindows = {}
     # for zone in [1, 2, 3, 4, 5]:
     for zone in [1]:
         # Creating a row just for the depot
@@ -203,21 +254,21 @@ def main():
     print(depotLocations)
 
     # # Creating jobs that need to be completed
-    # jobs = [{'zone': 4, 'numTrucks': 3, 'limit': 5600}, #
-    #         {'zone': 4, 'numTrucks': 4, 'limit': 4375}, #
+    # jobs = [{'zone': 4, 'numTrucks': 3, 'limit': 5600},  #
+    #         {'zone': 4, 'numTrucks': 4, 'limit': 4375},  #
     #         {'zone': 4, 'numTrucks': 5, 'limit': 3600},
     #         {'zone': 4, 'numTrucks': 6, 'limit': 3125},
-    #         {'zone': 4, 'numTrucks': 7, 'limit': 2750}] #
-    # jobs = [{'zone': 1, 'numTrucks': 3, 'limit': 6900},
-    #         {'zone': 1, 'numTrucks': 4, 'limit': 5325},
-    #         {'zone': 1, 'numTrucks': 5, 'limit': 4500},
-    #         {'zone': 1, 'numTrucks': 6, 'limit': 3900},
-    #         {'zone': 1, 'numTrucks': 7, 'limit': 3500}]
-    jobs = [{'zone': 2, 'numTrucks': 3, 'limit': 6900},
-            {'zone': 2, 'numTrucks': 4, 'limit': 5325},
-            {'zone': 2, 'numTrucks': 5, 'limit': 4500},
-            {'zone': 2, 'numTrucks': 6, 'limit': 3900},
-            {'zone': 2, 'numTrucks': 7, 'limit': 3500}]
+    #         {'zone': 4, 'numTrucks': 7, 'limit': 2750}]  #
+    jobs = [{'zone': 1, 'numTrucks': 3, 'limit': 6900, 'maxTimeWindow': 5400},
+            {'zone': 1, 'numTrucks': 4, 'limit': 5325, 'maxTimeWindow': 3600},
+            {'zone': 1, 'numTrucks': 5, 'limit': 4500, 'maxTimeWindow': 3600},
+            {'zone': 1, 'numTrucks': 6, 'limit': 3900, 'maxTimeWindow': 3600},
+            {'zone': 1, 'numTrucks': 7, 'limit': 3500, 'maxTimeWindow': 3600}]
+    # jobs = [{'zone': 2, 'numTrucks': 3, 'limit': 6900},
+    #         {'zone': 2, 'numTrucks': 4, 'limit': 5325},
+    #         {'zone': 2, 'numTrucks': 5, 'limit': 4500},
+    #         {'zone': 2, 'numTrucks': 6, 'limit': 3900},
+    #         {'zone': 2, 'numTrucks': 7, 'limit': 3500}]
 
     print("Defining threads")
     processes = []
@@ -232,7 +283,8 @@ def main():
                              job['limit'],
                              locations,
                              process,
-                             job['zone']
+                             job['zone'],
+                             job['maxTimeWindow']
                              )
                        )
         )
@@ -241,7 +293,7 @@ def main():
     for process in range(0, numProcesses):
         processes[process].start()
 
-    print('Joning the threads')
+    print('Joining the threads')
     # Joining the threads
     for process in range(0, numProcesses):
         processes[process].join()
